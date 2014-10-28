9dcd8e8cfc7a91bafe752ae8b8131ceac5f5abf8
ZeroGravira <gawallace25@gmail.com>
2014-10-28 15:06:32 -0400

    Applet now displays sort bars, but the sorting algorithm still hasnt been implemented.

--- Sort-Visualizer/SortVisualizerApplet.java
+++ Sort-Visualizer/SortVisualizerApplet.java
@@ -11,6 +11,8 @@ public class SortVisualizerApplet extends Applet implements Runnable {
 
 	private BufferedImage image;
 	private Graphics2D g2d;
+	private Thread mainloop;
+
 
 	@Override
 	public void init() {
@@ -21,24 +23,18 @@ public class SortVisualizerApplet extends Applet implements Runnable {
 
 	@Override
 	public void paint( Graphics g ) {
-
+		g.drawImage( image, 0, 0, this );
 	}
 
 	@Override
 	public void update( Graphics g ) {
-
+		paint( g );
 	}
 
 	@Override
 	public void start() {
-
-		/*populate and randomize array*/
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		for(int i = 1; i <= 40; i++ ) {
-			array.add( i );
-		}
-		Collections.shuffle( array );
-
+		mainloop = new Thread( this );
+		mainloop.start();
 	}
 
 	@Override
@@ -49,5 +45,56 @@ public class SortVisualizerApplet extends Applet implements Runnable {
 	@Override
 	public void run() {
 
+		/*populate and randomize array*/
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		for(int i = 1; i <= 40; i++ ) {
+			array.add( i );
+		}
+		Collections.shuffle( array );
+
+		updateVisualizer( array, 1, 2 );
+		repaint();
+		try {
+			Thread.sleep( 2000 );
+		} catch( Exception e ) {
+			System.err.println( "error!" );
+		}
+		updateVisualizer( array, 3, 5 );
+		try {
+			Thread.sleep( 2000 );
+		} catch( Exception e ) {
+			System.err.println( "error!" );
+		}
+		updateVisualizer( array, 7, 10 );
+		try {
+			Thread.sleep( 2000 );
+		} catch( Exception e ) {
+			System.err.println( "error!" );
+		}
+	}
+
+	public void updateVisualizer( ArrayList< Integer > array, int index1, int index2 ) {
+
+		int barHeight = IMG_HEIGHT/40-1;
+		int barUnitLength = (IMG_WIDTH/40);
+		
+		/*clear backbuffer*/
+		g2d.setColor( Color.WHITE );
+		g2d.fillRect( 0, 0, IMG_WIDTH, IMG_HEIGHT );
+
+		/*draw bars from the array list*/
+		g2d.setColor( Color.BLACK );
+		for( int i = 0; i < 40; i++ ) {
+			int barLength = barUnitLength*array.get(i);
+			int pos = (IMG_HEIGHT/40)*i;
+			g2d.fillRect( 0, pos, barLength, barHeight );
+		}
+
+		/*draw index lines*/
+		g2d.setColor( Color.RED );
+		g2d.fillRect( 0, (IMG_HEIGHT/40)*(index1+1)-1, IMG_WIDTH, 1 );
+		g2d.setColor( Color.BLUE );
+		g2d.fillRect( 0, (IMG_HEIGHT/40)*(index2+1)-1, IMG_WIDTH, 1 );
+		repaint();
 	}
 }
